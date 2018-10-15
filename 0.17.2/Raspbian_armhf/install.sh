972383fe3805622d92e300baf7c6115ef1800abd
PINTO0309 <rmsdh122@yahoo.co.jp>
2018-10-15 23:00:15 +0900

    update

---
+++ 0.17.2/Raspbian_armhf/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1RR2LAhMJM0z7Cxzah4CeLjJft8BJ9AjQ" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1RR2LAhMJM0z7Cxzah4CeLjJft8BJ9AjQ" -o bazel
+sudo cp ./bazel /usr/local/bin
---
+++ 0.17.2/Ubuntu1604_x86_64/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1dvR3pdM6vtkTWqeR-DpgVUoDV0EYWil5" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1dvR3pdM6vtkTWqeR-DpgVUoDV0EYWil5" -o bazel
+sudo cp ./bazel /usr/local/bin
