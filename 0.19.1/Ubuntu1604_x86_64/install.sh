84a0539a3d92feda8c4ba1a392f9bce455b210b2
PINTO0309 <rmsdh122@yahoo.co.jp>
2018-11-14 11:09:47 +0900

    update

---
+++ 0.19.1/Ubuntu1604_x86_64/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=13zEKn1pvVlPS-DjZcQNjYfAbcVkQK12L" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=13zEKn1pvVlPS-DjZcQNjYfAbcVkQK12L" -o bazel
+sudo cp ./bazel /usr/local/bin
+rm ./bazel
