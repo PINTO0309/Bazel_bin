978ceace822fb575c95f6ef6a30a0668e35eb920
PINTO0309 <rmsdh122@yahoo.co.jp>
2019-10-21 01:36:07 +0900

    corresponds glibc2.28

--- 0.26.1/Raspbian_Debian_Buster_aarch64/openjdk-8-jdk/install.sh
+++ 0.26.1/Raspbian_Debian_Buster_aarch64/openjdk-8-jdk/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=12q-6_lXVsHwQ09K1DA96LMtLuSZYqlR3" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1GRxgbECrXgqQvYGOg1DzYHxsPLjyk0e3" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=12q-6_lXVsHwQ09K1DA96LMtLuSZYqlR3" -o bazel
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1GRxgbECrXgqQvYGOg1DzYHxsPLjyk0e3" -o bazel
 sudo chmod +x bazel
 sudo cp ./bazel /usr/local/bin
 rm ./bazel
