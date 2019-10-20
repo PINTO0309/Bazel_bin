6d9fb759429a643211f6b5bfaedc7be531ff94c2
PINTO0309 <rmsdh122@yahoo.co.jp>
2019-10-20 19:26:51 +0900

    update

--- 0.26.1/Raspbian_Debian_Buster_armhf/openjdk-8-jdk/install.sh
+++ 0.26.1/Raspbian_Debian_Buster_armhf/openjdk-8-jdk/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1rsMmCwdJ5HmygFDtbiVyl2VchZLojp8i" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1Lew3JAlFANULjv3CD4mdcKlcsunfMwIE" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1rsMmCwdJ5HmygFDtbiVyl2VchZLojp8i" -o bazel
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1Lew3JAlFANULjv3CD4mdcKlcsunfMwIE" -o bazel
 sudo chmod +x bazel
 sudo cp ./bazel /usr/local/bin
 rm ./bazel
