c109044885994f627ce898668e40399ec2793598
PINTO0309 <rmsdh122@yahoo.co.jp>
2021-03-29 07:07:44 +0900

    3.7.2

---
+++ 3.7.2/armhf/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1s7zn7DbYtvcyC813LYGfJOD4bkDbqfXt" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1s7zn7DbYtvcyC813LYGfJOD4bkDbqfXt" -o bazel
+sudo chmod +x bazel
+sudo cp ./bazel /usr/local/bin
+rm ./bazel
